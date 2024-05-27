886643c2f97bb0d46d05e7c8d085b66fdb3621ff
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-05-27 15:13:46 +0100

    blah

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -1,7 +1,7 @@
 export default function FilterBar (){
     return (
         <div className="filter-container">
-            <h2>Filters 1</h2>            
+            <h2>Filters 1 2</h2>            
         </div>
     );
 }
