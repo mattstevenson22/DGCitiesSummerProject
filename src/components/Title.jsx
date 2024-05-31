f3deedbca0ab821498a383886fb72a0ce5671ef2
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-05-31 14:36:27 +0100

    added login box

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,76 @@
+import {useEffect, useState, useRef} from "react";
+
 export default function Title() {
+
+  //var to store whether login popup is shown or not
+  const [loginPopup, setLoginPopup] = useState(false);
+
+  const [user, setUser] = useState('');
+
+  const [pwd, setPwd] = useState('');
+
+  const [success, setSuccess] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(user,pwd);
+    setUser('');
+    setPwd('');
+    setSuccess(true);
+  }
+
+
+  // login popup itself
+  function LoginPopup(props) {
+
+    return (props.trigger) ? (
+        <div className="login-box">
+
+          {success ? (
+            <>
+            <p> success </p>
+            <button className="btn" onClick={() => props.setTrigger(false)}>Close</button>
+            </>
+          ) : (
+          <>
+          <p className="PopupTitleText"> Please log in: </p>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username:</label>
+            <input 
+              type="text"
+              id="username"
+              autoComplete="off"
+              onChange={(e) => setUser(e.target.value)}
+              value={user}
+              required 
+            />
+
+            <label htmlFor="password">Password:</label>
+            <input 
+              type="password"
+              id="password"
+              onChange={(e) => setPwd(e.target.value)}
+              value={pwd}
+              required 
+            />
+            <button> Log In </button>
+          </form>
+          <button className="btn" onClick={() => props.setTrigger(false)}>Close</button>
+          </>
+          )}
+        </div>
+    ) : "";
+  }
+
   return (
     <>
-      <h1 className="title"> Greenwich Complaints Portal </h1>
+      <ul>
+        <li className="title"> Greenwich Complaints Portal </li>
+        <li className="loginButton"> <button className="btn" onClick={()=>{setLoginPopup(true)}}>Log in</button></li>
+      </ul>
+
+      <LoginPopup trigger={loginPopup} setTrigger={setLoginPopup} />
+
     </>
   );
 }
