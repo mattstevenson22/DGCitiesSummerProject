6c5dd3dfc55cdaf977df038cd78552f76a5a8798
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-06-05 09:31:36 +0100

    adding backend call from api

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,94 +1,131 @@
 import CountUp from "react-countup";
 import { Pie } from "react-chartjs-2";
 import { Chart as ChartJS, Tooltip, Legend, ArcElement } from "chart.js";
+import { useEffect, useState } from "react";
+import api from "../api/backendapi";
 
-ChartJS.register(Tooltip, Legend, ArcElement); 
+ChartJS.register(Tooltip, Legend, ArcElement);
 
 export default function Dashboard() {
+  const [dashboardJson, setDashboardJson] = useState({});
 
-    const totalNoOfComplaints = 1485
-
-    const categoryPieChartData = {
-      labels: ["Category 1", "Category 2", "Category 3"],
-      datasets: [
-        {
-          label: "Complaints by Category",
-          data: [60,40,20],
-          backgroundColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54,162,235,1)",
-            "rgba(153,102,255,1)"
-          ],
-          hoverOffset: 4,
-        }
-      ]
+  useEffect(() => {
+    const fetchDashboardData = async () => {
+      try {
+        const res = await api.get("/dashboard");
+        console.log("Sending api request: /dashboard");
+        setDashboardJson(res.data);
+      } catch (err) {
+        console.log("Unsuccessful Request");
+      }
     };
+    fetchDashboardData();
+  }, []);
 
+  let totalComplaints = dashboardJson.total_complaints;
+  let catPCdata = dashboardJson.catPCdata;
+  let cat_most = dashboardJson.cat_most;
+  let cat_least = dashboardJson.cat_least;
+  let areaPCdata = dashboardJson.areaPCdata;
+  let area_most = dashboardJson.area_most;
+  let area_least = dashboardJson.area_least;
+  let resolution_data = dashboardJson.resolution_data;
 
-    const areaPieChartData = {
-      labels: ["Area 1", "Area 2", "Area 3"],
-      datasets: [
-        {
-          label: "Complaints by Area",
-          data: [100,10,52],
-          backgroundColor: [
-            "rgba(255,99,132,1)",
-            "rgba(54,162,235,1)",
-            "rgba(153,102,255,1)"
-          ],
-          hoverOffset: 4,
-        }
-      ]
-    };
+  let categoryPieChartData = {
+    labels: ["Category 1", "Category 2", "Category 3"],
+    datasets: [
+      {
+        label: "Complaints by Category",
+        data: catPCdata,
+        backgroundColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54,162,235,1)",
+          "rgba(153,102,255,1)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
 
-    const options = {
-      maintainAspectRatio: false,
-      responsive: true
-    }
+  let areaPieChartData = {
+    labels: ["Area 1", "Area 2", "Area 3"],
+    datasets: [
+      {
+        label: "Complaints by Area",
+        data: areaPCdata,
+        backgroundColor: [
+          "rgba(255,99,132,1)",
+          "rgba(54,162,235,1)",
+          "rgba(153,102,255,1)",
+        ],
+        hoverOffset: 4,
+      },
+    ],
+  };
 
+  const options = {
+    maintainAspectRatio: false,
+    responsive: true,
+  };
 
-    return (
-      <div className="dashboard-container">
-
-        <div className="dashboard-tile">
-           <p className="total-complaints-header"> Total Number of Complaints: </p>
-           <CountUp className="total-complaints-value" end={totalNoOfComplaints} />
-        </div>
-
-        <div className="dashboard-tile"> 
-          <p> Complaints by Category: </p>
-          <div className="piechart-container">
-            <Pie data={categoryPieChartData} options={options} />
-          </div>
-        </div>
-
-        <div className="dashboard-tile"> 
-          <p> Category with the <span className="bad"> most </span> complaints in prev. month: </p>
-          <p> Damp </p>
-          <p> Category with the <span className="good"> least </span> complaints in prev. month: </p>
-          <p> Parks </p>
-        </div>
-
-        <div className="dashboard-tile2">
-          <p> Complaints by Area: </p>
-          <div className="piechart-container">
-            <Pie data={areaPieChartData} options={options} />
-          </div>
-        </div>
-
-        <div className="dashboard-tile">
-          <p> Area with the <span className="bad"> most </span> complaints in prev. month: </p>
-          <p> Woolwich </p>
-          <p> Area with the <span className="good"> least </span> complaints in prev. month: </p>
-          <p> Blackheath </p>
-        </div>
-
-        <div className="dashboard-tile">
-           <p> Number of complaints resolved in prev. month, relative to past months: </p>
-           <p> i'm a bar chart </p>
-        </div>
-
+  return (
+    <div className="dashboard-container">
+      <div className="dashboard-tile">
+        <p className="total-complaints-header"> Total Number of Complaints: </p>
+        <CountUp className="total-complaints-value" end={totalComplaints} />
       </div>
-    );
-  }
-  
+
+      <div className="dashboard-tile">
+        <p> Complaints by Category: </p>
+        <div className="piechart-container">
+          <Pie data={categoryPieChartData} options={options} />
+        </div>
+      </div>
+
+      <div className="dashboard-tile">
+        <p>
+          {" "}
+          Category with the <span className="bad"> most </span> complaints in
+          prev. month:{" "}
+        </p>
+        <p> {cat_most} </p>
+        <p>
+          {" "}
+          Category with the <span className="good"> least </span> complaints in
+          prev. month:{" "}
+        </p>
+        <p> {cat_least} </p>
+      </div>
+
+      <div className="dashboard-tile2">
+        <p> Complaints by Area: </p>
+        <div className="piechart-container">
+          <Pie data={areaPieChartData} options={options} />
+        </div>
+      </div>
+
+      <div className="dashboard-tile">
+        <p>
+          {" "}
+          Area with the <span className="bad"> most </span> complaints in prev.
+          month:{" "}
+        </p>
+        <p> {area_most} </p>
+        <p>
+          {" "}
+          Area with the <span className="good"> least </span> complaints in
+          prev. month:{" "}
+        </p>
+        <p> {area_least} </p>
+      </div>
+
+      <div className="dashboard-tile">
+        <p>
+          {" "}
+          Number of complaints resolved in prev. month, relative to past months:{" "}
+        </p>
+        <p> {resolution_data} </p>
+      </div>
+    </div>
+  );
+}
--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,26 +2,23 @@
 import "leaflet/dist/leaflet.css";
 
 //Component imports
-import { MapContainer, TileLayer, Marker, Popup, useMap} from "react-leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import { Icon } from "leaflet";
 import MarkerClusterGroup from "react-leaflet-cluster";
 
 //Misc imports
 import pin from "../images/red-pin.png";
-import {useEffect, useState} from "react";
-import api from '../api/complaints';
+import { useEffect, useState } from "react";
+import api from "../api/backendapi";
 
-export default function Map( {filterSelection} ){
-
-
+export default function Map({ filterSelection }) {
   // ----- 1: Setup -----
-
 
   // marker icon
   const customIcon = new Icon({
-      iconUrl: pin,
-      iconSize: [30, 30],
-      iconAnchor: [15, 30]
+    iconUrl: pin,
+    iconSize: [30, 30],
+    iconAnchor: [15, 30],
   });
 
   // var to store the complaint data in json format
@@ -32,14 +29,13 @@ export default function Map( {filterSelection} ){
 
   // var to store state of current complaint key for use in generating the extra info popup
   const [currentComplaintKey, setCurrentComplaintKey] = useState("");
-  
-  // create an empty array to store the complaint markers 
+
+  // create an empty array to store the complaint markers
   let complaint_markers = [];
-  
 
   // retrieve the complaint data from server whenever the filter selection changes
   useEffect(() => {
-    const fetchData = async () => {
+    const fetchMapData = async () => {
       try {
         const filterSelectionArray = filterSelection.split("|");
         const urgency = filterSelectionArray[0];
@@ -47,43 +43,68 @@ export default function Map( {filterSelection} ){
         const category = filterSelectionArray[2];
         const responder = filterSelectionArray[3];
         const age = filterSelectionArray[4];
-        const response = await api.get("/data?urgency="+urgency+"&progress="+progress+"&category="+category+"&responder="+responder+"&age="+age);
-        console.log("Sending api request: /data?urgency="+urgency+"&progress="+progress+"&category="+category+"&responder="+responder+"&age="+age)
+        const response = await api.get(
+          "/data?urgency=" +
+            urgency +
+            "&progress=" +
+            progress +
+            "&category=" +
+            category +
+            "&responder=" +
+            responder +
+            "&age=" +
+            age
+        );
+        console.log(
+          "Sending api request: /data?urgency=" +
+            urgency +
+            "&progress=" +
+            progress +
+            "&category=" +
+            category +
+            "&responder=" +
+            responder +
+            "&age=" +
+            age
+        );
         setComplaintsJson(response.data);
       } catch (err) {
-        console.log('Unsuccessful Request');
+        console.log("Unsuccessful Request");
       }
-    }
-    fetchData();
-  }, [filterSelection])
-
+    };
+    fetchMapData();
+  }, [filterSelection]);
 
   // ----- 2: Functional components used within this component -----
 
-
   // maps the complaint markers array to actual marker components in react leaflet.
   function Markers({ data }) {
-
     const map = useMap();
 
-    return (
-      data.map((marker) => {
-        return (
-          <Marker key={marker.key} eventHandlers={{click: () => {map.setView(marker.geocode, 18)}}} position={marker.geocode} icon={customIcon}>
-            <Popup >{marker.popUp} </Popup>
-          </Marker>
-        );
-      })
-    );
+    return data.map((marker) => {
+      return (
+        <Marker
+          key={marker.key}
+          eventHandlers={{
+            click: () => {
+              map.setView(marker.geocode, 18);
+            },
+          }}
+          position={marker.geocode}
+          icon={customIcon}
+        >
+          <Popup>{marker.popUp} </Popup>
+        </Marker>
+      );
+    });
   }
 
   // displays more info about each complaint in the popup window
   function InfoPopup(props) {
-
     let complaint_info = complaintsJson[props.currentComplaintKey];
 
-    function getCorrectCategoryStyling(category){
-      if (category == "Damp" ) {
+    function getCorrectCategoryStyling(category) {
+      if (category == "Damp") {
         return "damp-category-tag";
       } else if (category == "Bins") {
         return "bin-category-tag";
@@ -92,12 +113,12 @@ export default function Map( {filterSelection} ){
       }
     }
 
-    function getCorrectSentimentStyling(sentiment){
-      if ( sentiment == "5" ) {
+    function getCorrectSentimentStyling(sentiment) {
+      if (sentiment == "5") {
         return "negative-sentiment-tag";
-      } else if ( sentiment == "4") {
+      } else if (sentiment == "4") {
         return "mildly-negative-sentiment-tag";
-      } else if ( sentiment == "3") {
+      } else if (sentiment == "3") {
         return "neutral-sentiment-tag";
       } else if (sentiment == "2") {
         return "mildly-positive-sentiment-tag";
@@ -105,92 +126,154 @@ export default function Map( {filterSelection} ){
         return "positive-sentiment-tag";
       }
     }
-    
 
-    return (props.trigger) ? (
-        <div className="additional-info-box">
-          <h3 className="AddInfoBoxTitleText"> {complaint_info.address} </h3>
-          <p className="AddInfoBoxRegularText"> Category: <span className={getCorrectCategoryStyling(complaint_info.category)}> {complaint_info.category} </span> </p>
-          <p className="AddInfoBoxRegularText"> Full Complaint: {complaint_info.full_complaint} </p>
-          <p className="AddInfoBoxRegularText"> Date: {complaint_info.timestamp} </p>
-          <p className="AddInfoBoxRegularText"> &#129668; Sentiment: <span className={getCorrectSentimentStyling(complaint_info.sentiment)}> {complaint_info.sentiment} </span> </p>
-          <p className="AddInfoBoxRegularText"> Email Address: {complaint_info.email} </p>
-          <p className="AddInfoBoxRegularText"> Telephone: {complaint_info.telephone} </p>
-          <button className="btn" onClick={() => props.setTrigger(false)}>Close</button>
-        </div>
-    ) : "";
+    return props.trigger ? (
+      <div className="additional-info-box">
+        <h3 className="AddInfoBoxTitleText"> {complaint_info.address} </h3>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          Category:{" "}
+          <span className={getCorrectCategoryStyling(complaint_info.category)}>
+            {" "}
+            {complaint_info.category}{" "}
+          </span>{" "}
+        </p>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          Full Complaint: {complaint_info.full_complaint}{" "}
+        </p>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          Date: {complaint_info.timestamp}{" "}
+        </p>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          &#129668; Sentiment:{" "}
+          <span
+            className={getCorrectSentimentStyling(complaint_info.sentiment)}
+          >
+            {" "}
+            {complaint_info.sentiment}{" "}
+          </span>{" "}
+        </p>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          Email Address: {complaint_info.email}{" "}
+        </p>
+        <p className="AddInfoBoxRegularText">
+          {" "}
+          Telephone: {complaint_info.telephone}{" "}
+        </p>
+        <button className="btn" onClick={() => props.setTrigger(false)}>
+          Close
+        </button>
+      </div>
+    ) : (
+      ""
+    );
   }
 
-
   // ----- 3: Preparing the Complaint Markers Array -----
-
 
   // iterate through each complaint in the JSON response and add a corresponding marker to the complaint markers array
   for (let c_key in complaintsJson) {
     if (complaintsJson.hasOwnProperty(c_key)) {
-        let complaint = complaintsJson[c_key];
+      let complaint = complaintsJson[c_key];
 
-        //choose correct category styling
+      //choose correct category styling
 
-        let correctCategoryColour;
+      let correctCategoryColour;
 
-        if ( complaint.category == "Damp" ) {
-          correctCategoryColour = "damp-category-tag";
-        } else if (complaint.category == "Bins") {
-          correctCategoryColour = "bin-category-tag";
-        } else {
-          correctCategoryColour = "neighbour-category-tag";
-        }
-        
-        // choose correct sentiment styling
-        
-        let correctSentimentColour;
+      if (complaint.category == "Damp") {
+        correctCategoryColour = "damp-category-tag";
+      } else if (complaint.category == "Bins") {
+        correctCategoryColour = "bin-category-tag";
+      } else {
+        correctCategoryColour = "neighbour-category-tag";
+      }
 
-        if ( complaint.sentiment == "5" ) {
-          correctSentimentColour = "negative-sentiment-tag";
-        } else if (complaint.sentiment == "4") {
-          correctSentimentColour = "mildly-negative-sentiment-tag";
-        } else if (complaint.sentiment == "3") {
-          correctSentimentColour = "neutral-sentiment-tag";
-        } else if (complaint.sentiment == "2") {
-          correctSentimentColour = "mildly-positive-sentiment-tag";
-        } else {
-          correctSentimentColour = "positive-sentiment-tag";
-        }
+      // choose correct sentiment styling
 
-        complaint_markers.push({
-            key: c_key,
-            geocode: complaint.geocode,
-            popUp:  <> <h4 className="PopupTitleText"> {complaint.address} </h4> <p className="PopupRegularText"> Category: <span className={correctCategoryColour}> {complaint.category} </span> </p> <p className="PopupRegularText"> &#129668; Summary: {complaint.summary} </p> <p className="PopupRegularText"> &#129668; Urgency: <span className={correctSentimentColour}> {complaint.sentiment} </span> </p> <button className="btn" onClick={() => {setCurrentComplaintKey(c_key); setButtonPopup(true);}}> See more </button> </>
+      let correctSentimentColour;
+
+      if (complaint.sentiment == "5") {
+        correctSentimentColour = "negative-sentiment-tag";
+      } else if (complaint.sentiment == "4") {
+        correctSentimentColour = "mildly-negative-sentiment-tag";
+      } else if (complaint.sentiment == "3") {
+        correctSentimentColour = "neutral-sentiment-tag";
+      } else if (complaint.sentiment == "2") {
+        correctSentimentColour = "mildly-positive-sentiment-tag";
+      } else {
+        correctSentimentColour = "positive-sentiment-tag";
+      }
+
+      complaint_markers.push({
+        key: c_key,
+        geocode: complaint.geocode,
+        popUp: (
+          <>
+            {" "}
+            <h4 className="PopupTitleText"> {complaint.address} </h4>{" "}
+            <p className="PopupRegularText">
+              {" "}
+              Category:{" "}
+              <span className={correctCategoryColour}>
+                {" "}
+                {complaint.category}{" "}
+              </span>{" "}
+            </p>{" "}
+            <p className="PopupRegularText">
+              {" "}
+              &#129668; Summary: {complaint.summary}{" "}
+            </p>{" "}
+            <p className="PopupRegularText">
+              {" "}
+              &#129668; Urgency:{" "}
+              <span className={correctSentimentColour}>
+                {" "}
+                {complaint.sentiment}{" "}
+              </span>{" "}
+            </p>{" "}
+            <button
+              className="btn"
+              onClick={() => {
+                setCurrentComplaintKey(c_key);
+                setButtonPopup(true);
+              }}
+            >
+              {" "}
+              See more{" "}
+            </button>{" "}
+          </>
+        ),
       });
-    }}
-
+    }
+  }
 
   // ----- 4: return JSX -----
-
 
   return (
     <>
       <MapContainer center={[51.476852, 0.015]} zoom={13.5}>
-
         <TileLayer
           attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url='https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png' />
+          url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
+        />
 
-        
-        <MarkerClusterGroup maxClusterRadius={30} chunkedLoading> 
-
+        <MarkerClusterGroup maxClusterRadius={30} chunkedLoading>
           <Markers data={complaint_markers} />
-
         </MarkerClusterGroup>
-
       </MapContainer>
 
-      <InfoPopup trigger={buttonPopup} setTrigger={setButtonPopup} currentComplaintKey={currentComplaintKey} />
+      <InfoPopup
+        trigger={buttonPopup}
+        setTrigger={setButtonPopup}
+        currentComplaintKey={currentComplaintKey}
+      />
     </>
   );
 }
-
 
 // Notes:
 // when giving a co-ordinate, latitude (north or south) comes before longitude (east or west).
