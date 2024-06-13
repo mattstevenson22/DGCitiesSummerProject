57ce89b4b67cbf95fd32fa2203648f44aec6fd06
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-06-13 11:16:34 +0100

    tiny changes

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -242,7 +242,8 @@ export default function Map({ filterSelection }) {
 
   return (
     <>
-      <MapContainer center={[51.476852, 0.015]} zoom={13.5}  scrollWheelZoom={false} smoothWheelZoom={true} smoothSensitivity={1} >
+      <MapContainer center={[51.476852, 0.015]} zoom={13.5}>
+      {/* <MapContainer center={[51.476852, 0.015]} zoom={13.5}  scrollWheelZoom={false} smoothWheelZoom={true} smoothSensitivity={1} > */}
         <TileLayer
           attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://tiles.stadiamaps.com/tiles/outdoors/{z}/{x}/{y}{r}.png"
