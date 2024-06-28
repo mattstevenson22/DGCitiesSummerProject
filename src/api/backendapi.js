3123b2d1e93aee3b5f4a37465f05aeaddf833620
Matthew Stevenson <matthew.stevenson21@imperial.ac.uk>
2024-06-28 08:49:52 +0100

    removing mock server

--- src/api/backendapi.js
+++ src/api/backendapi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL: 'http://146.169.137.54:5000'
+    baseURL: 'http://[backendipaddress]:5000'
     // baseURL: 'http://localhost:3500'
 });
